d6e156bcb3c90289020d97ca23230ebb574ddb2b
nata romero <cuquinata267-afk>
2025-09-18 19:58:02 -0400

    ORDER MODEL

---
+++ WebApplication1/WebApplication1/Models/Order.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace WebApplication1.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        [Required]
+        public DateTime FechaPedido { get; set; } = DateTime.Now;
+
+        [Required]
+        public OrderStatus Estado { get; set; } = OrderStatus.Pendiente;
+
+        [Range(0, double.MaxValue)]
+        public decimal Total { get; set; }
+
+        [StringLength(500)]
+        public string Notas { get; set; }
+
+        // Navegación
+        public virtual User User { get; set; }
+        public virtual ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+    }
+
+    public enum OrderStatus
+    {
+        Pendiente,
+        Procesado,
+        Enviado,
+        Entregado,
+        Cancelado
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Product.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace WebApplication1.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Name { get; set; }
+
+        [StringLength(1000)]
+        public string Descripcion { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "El precio debe ser mayor a 0")]
+        public decimal Precio { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue, ErrorMessage = "El stock no puede ser negativo")]
+        public int Stock { get; set; }
+
+        [StringLength(100)]
+        public string Categoria { get; set; }
+
+        public bool EstaActivo { get; set; } = true;
+        public DateTime Creado { get; set; } = DateTime.Now;
+
+        // Navegación
+        public virtual ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+    }
+}
